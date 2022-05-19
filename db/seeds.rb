e4ab6507716280f085d90e0b529de661e1983a58
Tomas Quintanilha <tquinta@alumni.ie.edu>
2022-05-19 17:05:07 +0200

    basic back end complete. working model. Next is styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Deleting all previous items'
+
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts 'Deleting complete'
+puts 'Creating movies, lists, and bookmarks for you'
+
+10.times do
+  url = 'https://api.lorem.space/image?w=150&h=180'
+  movie = Movie.new(
+    title: Faker::Kpop.iii_groups,
+    overview: Faker::Quote.yoda,
+    poster_url: url,
+    rating: rand(0.0..5.0).round(1)
+  )
+  movie.save!
+
+  puts "created #{movie.title}"
+
+  list = List.new(
+    name: Faker::Music.album
+  )
+  list.save!
+  puts "created #{list.name}"
+end
+
+puts 'Created movies and lists'
